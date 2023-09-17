15888a765089814906b95e13eba16d4180adb67b
ringztech01 <145074523+ringztech01@users.noreply.github.com>
2023-09-17 17:27:47 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,17 +7,24 @@ ar typed =new Typed(".typing",{
 })
 
 function sendEmail(){
-     Email.send({
-        secureToken: "7269333c-df91-4763-87d1-cc1d7e133457",  
+   
+    Email.send({
+        
+      Host : "smtp.elasticemail.com",
+        Username : "ringzsystems@gmail.com",
+        Password : "7A7490BECCD59CD61F4C9652EAFF1DD384AF",
         To : 'ringzsystems@gmail.com',
         From : 'ringzsystems@gmail.com',
         ReplyFrom : document.getElementById("email").value,
         Subject : document.getElementById("subject").value,
-        Body :  document.getElementById("name").value 
-        + "Email: " + document.getElementById("email").value
-        + "Subject: " + document.getElementById("subject").value
-        + "Message: " + document.getElementById("message").value
+        Body : "Name: " + document.getElementById("name").value 
+        + "<br>Email: " + document.getElementById("email").value
+        + "<br>Subject: " + document.getElementById("subject").value
+        + "<br>Message: " + document.getElementById("message").value
+
     }).then(
-      message => alert( "message")
+      message => alert("Thankyou," + document.getElementById("name").value + "  your message has been sent")
     );
+
+
 }
