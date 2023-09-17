c4da8b976ae2a3578354ca6b34ea164121098ddc
ringztech01 <145074523+ringztech01@users.noreply.github.com>
2023-09-17 04:25:42 +0000

    Update script.js
    SMTP and encryption

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ var typed =new Typed(".typing",{
     smartBackspace: true,
     loop: true
 })
+
+function sendEmail(){
+   
+    Email.send({
+        SecureToken: "df5811bf-157f-4013-b689-0e9f76d43b36",
+        To : 'ringzsystems@gmail.com',
+        From : 'ringzsystems@gmail.com',
+        ReplyFrom : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : document.getElementById("message").value 
+        + "Email: " + document.getElementById("email").value
+        + "Subject: " + document.getElementById("subject").value
+        + "Message: " + document.getElementById("message").value
+
+    }).then(
+      message => alert("Thankyou," + document.getElementById("name").value + "  your message has been sent")
+    );
+
+
+}
