3b4920acbbb165045d32d0e1e5d20a72e5c4f682
ringztech01 <145074523+ringztech01@users.noreply.github.com>
2023-09-17 05:31:50 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,9 +15,12 @@ function sendEmail(){
         ReplyFrom : document.getElementById("email").value,
         Subject : document.getElementById("subject").value,
         Body : document.getElementById("name").value
-        + "<br> Email: " + document.getElementById("email").value
-        + "<br> Subject: " + document.getElementById("subject").value
-        + "<br> Message: " + document.getElementById("message").value
+            document.write("\n");
+        + "Email: " + document.getElementById("email").value
+            document.write("\n");
+        + "Subject: " + document.getElementById("subject").value
+            document.write("\n");
+        + "Message: " + document.getElementById("message").value
 
     }).then(
       message => alert("Thankyou," + document.getElementById("name").value + "  your message has been sent")
