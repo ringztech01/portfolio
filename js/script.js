9c79fb759167d450342fb456469ff07f1b1424da
ringztech01 <145074523+ringztech01@users.noreply.github.com>
2023-09-17 06:29:13 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,11 +11,11 @@ function sendEmail(){
     Email.send({
         SecureToken: "df5811bf-157f-4013-b689-0e9f76d43b36",
         To : 'ringzsystems@gmail.com',
-        From : 'ringztech01@gmail.com',
+        From : 'ringzsystems@gmail.com',
         ReplyFrom : document.getElementById("email").value,
         Subject : document.getElementById("subject").value,
-        Body : "Name: "document.getElementById("name").value + "Email: " + document.getElementById("email").value 
-        + "Subject: " + document.getElementById("subject").value + "Message: " + document.getElementById("message").value
+        Body : "Name: "document.getElementById("name").value + "<br>Email: " + document.getElementById("email").value 
+        + "<br>Subject: " + document.getElementById("subject").value + "<br>Message: " + document.getElementById("message").value
 
     }).then(
       message => alert("Thankyou," + document.getElementById("name").value + "  your message has been sent")
